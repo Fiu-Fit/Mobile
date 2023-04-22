dc2811aacc9d4a48bed27d7c73625b15a39faffe
Ezequiel Schejtman <ZequiceteYT@gmail.com>
2023-04-22 14:42:05 -0300

    Add Dark & Light Theme logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import AuthStack from './navigation/auth-navigator';
 import axios from 'axios';
+import {
+  Provider as PaperProvider,
+  MD3DarkTheme,
+  MD3LightTheme,
+  adaptNavigationTheme,
+} from 'react-native-paper';
+import { useColorScheme } from 'react-native';
 
 axios.defaults.baseURL = 'http://localhost:8080'; // API Gateway URL
 
 const App = () => {
+  const colorScheme = useColorScheme();
+  const { DarkTheme } = adaptNavigationTheme({
+    reactNavigationDark: DefaultTheme,
+  });
+  const { LightTheme } = adaptNavigationTheme({
+    reactNavigationLight: DefaultTheme,
+  });
+
+  const paperTheme = colorScheme === 'dark' ? MD3DarkTheme : MD3LightTheme;
+  const rNavTheme = colorScheme === 'dark' ? DarkTheme : LightTheme;
   return (
-    <NavigationContainer>
-      <AuthStack />
-    </NavigationContainer>
+    <PaperProvider theme={paperTheme}>
+      <NavigationContainer theme={rNavTheme}>
+        <AuthStack />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
