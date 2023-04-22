0832fe6ab2489ba289f1132720b39e3ab543902d
mmedela <mmedela@fi.uba.ar>
2023-04-22 16:44:47 -0300

    refactor navBar

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {Appbar, TextInput} from 'react-native-paper';
+import { Appbar, TextInput } from 'react-native-paper';
+import AddButton from '../addButton';
 
 const NavBar = () => {
   const [filterText, setFilterText] = React.useState('');
-  const onChangeSearch = userInput => setFilterText(userInput);
+  const onChangeSearch = (userInput: string): void => setFilterText(userInput);
   return (
     <Appbar.Header>
-      <Appbar.Content title="filter" />
+      <AddButton />
       <TextInput
-        placeholder="Search"
+        placeholder='Search'
         value={filterText}
         onChangeText={onChangeSearch}
-        style={{flex: 2, marginLeft: 16, marginRight: 16}}
+        style={{ flex: 2, marginLeft: 16, marginRight: 16 }}
       />
     </Appbar.Header>
   );
