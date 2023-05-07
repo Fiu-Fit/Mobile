117c0635873edfbe84d8c09beb9a419ca075a746
Ezequiel Schejtman <ZequiceteYT@gmail.com>
2023-05-07 17:06:14 -0300

    Use workoutStore to show workout cards info

--- src/screens/workouts/workouts.tsx
+++ src/screens/workouts/workouts.tsx
@@ -3,27 +3,10 @@ import { useAppTheme } from '../../App';
 import WorkoutCardList from '../../components/workoutCardList';
 import NavBar from '../../components/navBar';
 import { WorkoutsScreenNavigationProp } from '../../navigation/navigation-props';
+import { observer } from 'mobx-react';
+import { WorkoutStore } from '../../stores/workout.store';
 
-const cards = [
-  { id: 1, title: 'Card 1', content: 'card 1' },
-  { id: 2, title: 'Card 2', content: 'card 2' },
-  { id: 3, title: 'Card 3', content: 'card 3' },
-  { id: 4, title: 'Card 4', content: 'card 4' },
-  { id: 5, title: 'Card 5', content: 'card 5' },
-  { id: 6, title: 'Card 6', content: 'card 6' },
-  { id: 7, title: 'Card 7', content: 'card 7' },
-  { id: 8, title: 'Card 8', content: 'card 8' },
-  { id: 9, title: 'Card 9', content: 'card 9' },
-  { id: 10, title: 'Card 10', content: 'card 10' },
-  { id: 11, title: 'Card 10', content: 'card 11' },
-  { id: 12, title: 'Card 10', content: 'card 12' },
-  { id: 13, title: 'Card 10', content: 'card 13' },
-  { id: 14, title: 'Card 10', content: 'card 14' },
-  { id: 15, title: 'Card 10', content: 'card 15' },
-  { id: 16, title: 'Card 10', content: 'card 16' },
-  { id: 17, title: 'Card 10', content: 'card 17' },
-  { id: 18, title: 'Card 10', content: 'card 18' },
-];
+const workoutsStore = new WorkoutStore();
 
 const WorkoutsScreen = ({
   navigation,
@@ -34,9 +17,9 @@ const WorkoutsScreen = ({
   return (
     <View style={{ backgroundColor: appTheme.colors.background, flex: 1 }}>
       <NavBar navigation={navigation} />
-      <WorkoutCardList workouts={cards} />
+      <WorkoutCardList workouts={workoutsStore.cardsInfo} />
     </View>
   );
 };
 
-export default WorkoutsScreen;
+export default observer(WorkoutsScreen);
