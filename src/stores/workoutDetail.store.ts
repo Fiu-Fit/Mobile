0e6d481f6c4774a72a25e8de61df2317063df606
Ezequiel Schejtman <ZequiceteYT@gmail.com>
2023-05-10 20:18:27 -0300

    Add workoutDetailStore

---
+++ src/stores/workoutDetail.store.ts
@@ -0,0 +1,93 @@
+import { makeObservable, observable, computed, flow, runInAction } from 'mobx';
+import { CategoryType, WorkoutProps } from '../utils/workout-types';
+import { axiosClient } from '../utils/constants';
+import { IExerciseCard } from '../components/exerciseCard/exerciseCard';
+import LoggerFactory from '../utils/logger-utility';
+
+const logger = LoggerFactory('exercise-store');
+
+const defaultWorkout = {
+  _id: '',
+  name: '',
+  description: '',
+  duration: 0,
+  difficulty: 0,
+  category: CategoryType.LEGS,
+  rating: 0,
+  exercises: [],
+  athleteIds: [],
+  authorId: 0,
+};
+
+type IWorkoutHeader = {
+  name: string;
+  description: string;
+  duration: number;
+  globalRating: number;
+  exerciseCount: number;
+};
+
+export class WorkoutDetailStore {
+  workoutId: string | undefined;
+  workout: WorkoutProps = defaultWorkout;
+  state = 'pending';
+
+  get workoutHeader(): IWorkoutHeader {
+    const {
+      name,
+      description,
+      duration,
+      rating: globalRating,
+      exercises,
+    } = this.workout;
+    return {
+      name,
+      description,
+      duration,
+      globalRating,
+      exerciseCount: exercises.length,
+    };
+  }
+  get cardsInfo(): IExerciseCard[] {
+    return this.workout.exercises.map((workoutExercise): IExerciseCard => {
+      return {
+        id: workoutExercise.exerciseId,
+        name: workoutExercise.name,
+        description: workoutExercise.description,
+        category: workoutExercise.category.toString(),
+        sets: workoutExercise.sets.toString(),
+        reps: workoutExercise.reps.toString(),
+      };
+    });
+  }
+
+  constructor() {
+    makeObservable(this, {
+      workoutId: observable,
+      workout: observable,
+      state: observable,
+      cardsInfo: computed,
+      workoutHeader: computed,
+      fetchWorkout: flow,
+    });
+  }
+
+  *fetchWorkout(workoutId: string) {
+    this.state = 'pending';
+    try {
+      logger.debug('Getting workouts...');
+      const { data } = yield axiosClient.get<WorkoutProps>(
+        `/workouts/${workoutId}`,
+      );
+      logger.debug('Got data: ', data);
+      runInAction(() => {
+        this.workout = data;
+        this.state = 'done';
+      });
+    } catch (e) {
+      runInAction(() => {
+        this.state = 'error';
+      });
+    }
+  }
+}
