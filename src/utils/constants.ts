f61876320d24dc711f389d0e1b80362b3c12d453
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2023-05-20 13:38:00 -0300

    working local

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const API_URL = 'http://192.168.1.44:7071';
+export const API_URL = 'http://localhost:7071';
 
 export const axiosClient = axios.create({ baseURL: API_URL });
 
