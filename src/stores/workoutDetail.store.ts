d0463a9c1dd53018ffe3a3230f70795412851fcb
Ezequiel Schejtman <ZequiceteYT@gmail.com>
2023-05-20 16:48:52 -0300

    Refactor exercise listo into Map

--- src/stores/workoutDetail.store.ts
+++ src/stores/workoutDetail.store.ts
@@ -1,4 +1,11 @@
-import { makeObservable, observable, computed, flow, runInAction } from 'mobx';
+import {
+  makeObservable,
+  observable,
+  computed,
+  flow,
+  runInAction,
+  action,
+} from 'mobx';
 import {
   CategoryType,
   ExerciseCardInfo,
@@ -17,9 +24,9 @@ const defaultWorkout = {
   description: '',
   duration: 0,
   difficulty: 0,
-  category: CategoryType.LEGS,
+  category: CategoryType.FULLBODY,
   rating: { globalRating: 0, comments: [] },
-  exercises: [],
+  exercises: new Map<string, WorkoutExercise>(),
   athleteIds: [],
   authorId: 0,
 };
@@ -43,25 +50,27 @@ export class WorkoutDetailStore {
           'Qué gran entrenamiento. Los ejercicios fueron divertidos y me mantuvieron comprometido durante toda la sesión. Me encantó la combinación de fuerza y cardio. ¡Me siento enérgico y revitalizado!',
         ],
       },
-      exerciseCount: exercises.length,
+      exerciseCount: exercises.size,
     };
   }
 
   get exerciseCards(): ExerciseCardInfo[] {
-    return this.workout.exercises.map((workoutExercise): ExerciseCardInfo => {
-      return {
-        id: workoutExercise.exerciseId,
-        title: workoutExercise.exercise?.name ?? 'Name missing',
-        content: `${workoutExercise.sets} x ${workoutExercise.reps}`,
-        exercise: workoutExercise.exercise,
-        imageUrl:
-          'https://static.vecteezy.com/system/resources/previews/009/665/172/original/man-doing-sit-up-exercise-for-abdominal-muscles-vector-young-boy-wearing-a-blue-shirt-flat-character-athletic-man-doing-sit-ups-for-the-belly-and-abdominal-exercises-men-doing-crunches-in-the-gym-free-png.png',
-      };
-    });
+    return Array.from(this.workout.exercises.values()).map(
+      (workoutExercise): ExerciseCardInfo => {
+        return {
+          id: workoutExercise.exerciseId,
+          title: workoutExercise.exercise?.name ?? 'Name missing',
+          content: `${workoutExercise.sets} x ${workoutExercise.reps}`,
+          exercise: workoutExercise.exercise,
+          imageUrl:
+            'https://static.vecteezy.com/system/resources/previews/009/665/172/original/man-doing-sit-up-exercise-for-abdominal-muscles-vector-young-boy-wearing-a-blue-shirt-flat-character-athletic-man-doing-sit-ups-for-the-belly-and-abdominal-exercises-men-doing-crunches-in-the-gym-free-png.png',
+        };
+      },
+    );
   }
 
   get exercises(): WorkoutExercise[] {
-    return this.workout.exercises;
+    return Array.from(this.workout.exercises.values());
   }
 
   constructor() {
@@ -71,12 +80,20 @@ export class WorkoutDetailStore {
       state: observable,
       exerciseCards: computed,
       workoutHeader: computed,
+      addExercise: action,
+      editExercise: action,
+      removeExercise: action,
       fetchWorkout: flow,
     });
   }
 
   *fetchWorkout(workoutId: string) {
     this.state = 'pending';
+    if (!workoutId || workoutId === '') {
+      this.workout = defaultWorkout;
+      this.state = 'done';
+      return;
+    }
     try {
       logger.debug('Getting workout detail...');
       const { data } = yield axiosClient.get<WorkoutProps>(
@@ -84,7 +101,24 @@ export class WorkoutDetailStore {
       );
       logger.debug('Got data: ', data);
       runInAction(() => {
-        this.workout = data;
+        const {
+          exercises: exercisesList,
+        }: {
+          exercises: WorkoutExercise[];
+        } = data;
+        const workoutData = data;
+        this.workout = {
+          ...workoutData,
+          exercises: new Map<string, WorkoutExercise>(),
+        };
+        logger.info('Workout Data:', workoutData);
+        this.workout.exercises = exercisesList.reduce((map, exercise) => {
+          if (map.has(exercise.exerciseId)) {
+            return map;
+          }
+          map.set(exercise.exerciseId, exercise);
+          return map;
+        }, this.workout.exercises);
         logger.debug('Loaded Workout: ', this.workout);
         this.state = 'done';
       });
@@ -93,6 +127,17 @@ export class WorkoutDetailStore {
         this.state = 'error';
       });
     }
+  }
+  addExercise(exercise: WorkoutExercise) {
+    this.workout.exercises.set(exercise.exerciseId, exercise);
+  }
+
+  editExercise(exerciseId: string, exercise: WorkoutExercise) {
+    this.workout.exercises.set(exerciseId, exercise);
+  }
+
+  removeExercise(exerciseId: string) {
+    this.workout.exercises.delete(exerciseId);
   }
 }
 
