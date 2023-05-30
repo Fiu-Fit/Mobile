ea653e4cb0eb4e9017a9ca7bd04e8e2e6acfca23
mmedela <mmedela@fi.uba.ar>
2023-05-30 20:13:47 -0300

    add profile search to navigator

--- src/navigation/main-navigator.tsx
+++ src/navigation/main-navigator.tsx
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import WorkoutStack from './workout-navigator';
 import GoalStack from './goal-navigator';
+import ProfileSearchScreen from '../screens/profileSearchScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +14,7 @@ const iconMap = new Map([
   ['Profile', 'account'],
   ['Goals', 'trophy'],
   ['Workouts', 'dumbbell'],
+  ['Search', 'account-search'],
 ]);
 
 const tabBarIcon = ({
@@ -44,6 +46,7 @@ const TabNavigator = () => {
       <Tab.Screen name='Profile' component={UserProfile} />
       <Tab.Screen name='Goals' component={GoalStack} />
       <Tab.Screen name='Workouts' component={WorkoutStack} />
+      <Tab.Screen name='Search' component={ProfileSearchScreen} />
     </Tab.Navigator>
   );
 };
