72ef3c4193f18c3afcba5bffea5ed5214ab48b50
mmedela <mmedela@fi.uba.ar>
2023-06-06 00:35:30 -0300

    add search by distance functionality to store

--- src/stores/userSearch.store.ts
+++ src/stores/userSearch.store.ts
@@ -13,26 +13,54 @@ import LoggerFactory from '../utils/logger-utility';
 import { CardInfo, User } from '../utils/custom-types';
 
 const logger = LoggerFactory('user-search-store');
+
+type FilterOprions = 'name' | 'distance';
+
 class SearchStore {
   query: string = '';
   lastQuery: string | undefined = undefined;
   results: User[] = [];
   isLoading: boolean = false;
   cancelTokenSource: CancelTokenSource | undefined = undefined;
+  distance: number | undefined = undefined;
+  lastDistance: number | undefined = undefined;
+  filterOption: FilterOprions = 'name';
+
 
   constructor() {
     makeObservable(this, {
       query: observable,
+      distance: observable,
       results: observable,
+      filterOption: observable,
       cardsInfo: computed,
       isLoading: observable,
       setSearchQuery: action,
-      search: action,
+      setDistanceSearch: action,
+      searchByName: action,
+      switchSearchCriteria: action,
     });
   }
 
   setSearchQuery(query: string) {
     this.query = query;
+  }
+
+  setDistanceSearch(distance: number){
+    this.distance = distance;
+  }
+
+  switchSearchCriteria(criteria: FilterOprions){
+      if(criteria === 'name'){
+        this.lastDistance = undefined;
+        this.distance = undefined;
+      }
+      if(criteria === 'distance'){
+        this.query = '';
+        this.lastQuery = undefined;
+      }
+      this.filterOption = criteria;
+
   }
 
   get cardsInfo(): CardInfo[] {
@@ -48,18 +76,9 @@ class SearchStore {
     );
   }
 
-  search = debounce(async () => {
-    if (!this.query) {
-      runInAction(() => {
-        this.results = [];
-      });
-      return;
-    }
-
-    if (this.lastQuery && this.lastQuery === this.query) {
-      return;
-    }
-
+  searchByName = debounce(async () => {
+    if (!this.query || (this.lastQuery && this.lastQuery === this.query)) return;
+    
     if (this.cancelTokenSource) {
       this.cancelTokenSource.cancel('Request canceled');
     }
@@ -72,6 +91,46 @@ class SearchStore {
     this.results = [];
     try {
       const response = await axiosClient.get(`/users?params=${this.query}`, {
+        cancelToken: this.cancelTokenSource?.token,
+      });
+      logger.info('Response: ', response);
+
+      runInAction(() => {
+        logger.info('Setting: ', response.data);
+        this.results = response?.data.rows ?? [];
+      });
+    } catch (error) {
+      if (!axios.isCancel(error)) {
+        logger.error('Error occurred while searching:', error);
+        runInAction(() => {
+          logger.info('Setting: ', []);
+          this.results = [];
+        });
+      }
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+        this.cancelTokenSource = undefined;
+      });
+    }
+  }, 500);
+
+  searchByDistance = debounce(async (id: number) => {
+    
+    if (!this.distance || (this.lastDistance && this.lastDistance === this.distance)) return;
+
+    if (this.cancelTokenSource) {
+      this.cancelTokenSource.cancel('Request canceled');
+    }
+
+    runInAction(() => {
+      this.isLoading = true;
+      this.lastDistance = this.distance;
+    });
+    this.cancelTokenSource = axios.CancelToken.source();
+    this.results = [];
+    try {
+      const response = await axiosClient.get(`/users/${id}/nearest-trainers?radius=${this.distance}`, {
         cancelToken: this.cancelTokenSource?.token,
       });
       logger.info('Response: ', response);
