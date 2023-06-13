266b5dc41196112496b799f84b5dfeba50bb0400
mmedela <mmedela@fi.uba.ar>
2023-06-13 18:43:01 -0300

    add custom hook

--- src/utils/fetch-helpers.tsx
+++ src/utils/fetch-helpers.tsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from 'react';
 import { axiosClient } from './constants';
 import { User } from './custom-types';
 import LoggerFactory from './logger-utility';
 
 const logger = LoggerFactory('fetch-helpers');
 
-export const updateCurrentUser = async () => {
-  const { data } = await axiosClient.post('/users/me');
-  logger.info('Got User ID: ', data.id);
-  const { data: followedUsers } = await axiosClient.get(
-    `/followers/following?userId=${data.id}`,
-  );
-  logger.info('Got followedUsers: ', followedUsers);
-  return {
-    ...data,
-    followedUsers: followedUsers.rows,
-  } as User;
+export const useFetchUser = (id?: number ) => {
+  const [response, setResponse] = useState<any>(undefined);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } =  id ? await axiosClient.get('/users/me') : await axiosClient.get(`/users/${id}`);
+        logger.info('Got me: ', data);
+        const { data: followedUsers } = await axiosClient.get(
+          `/followers/following?userId=${data.id}`,
+        );
+        logger.info(`Got followedUsers for id: ${id}: `, followedUsers);
+        setResponse({
+          ...data,
+          followedUsers: followedUsers.rows,
+        } as User);
+      } catch (error: any) {
+        logger.error('An error ocurred while fetching the user: ', error);
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { response, error };
 };
