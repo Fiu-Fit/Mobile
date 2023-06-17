7c6b8ce51a8d8932b15f07426efb463b37e13d6d
Ezequiel Schejtman <ZequiceteYT@gmail.com>
2023-06-17 16:39:09 -0300

    refactor custom hook

--- src/utils/fetch-helpers.tsx
+++ src/utils/fetch-helpers.tsx
@@ -1,34 +1,44 @@
 import { useEffect, useState } from 'react';
 import { axiosClient } from './constants';
-import { User } from './custom-types';
 import LoggerFactory from './logger-utility';
+import { User } from './custom-types';
 
 const logger = LoggerFactory('fetch-helpers');
 
-export const useFetchUser = (id?: number ) => {
-  const [response, setResponse] = useState<any>(undefined);
+export const useFetchUser = (id?: number) => {
+  const [response, setResponse] = useState<User | undefined>(undefined);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const { data } =  id ? await axiosClient.get('/users/me') : await axiosClient.get(`/users/${id}`);
-        logger.info('Got me: ', data);
-        const { data: followedUsers } = await axiosClient.get(
-          `/followers/following?userId=${data.id}`,
-        );
-        logger.info(`Got followedUsers for id: ${id}: `, followedUsers);
-        setResponse({
-          ...data,
-          followedUsers: followedUsers.rows,
-        } as User);
-      } catch (error: any) {
-        logger.error('An error ocurred while fetching the user: ', error);
-        setError(error);
-      }
+      const { response, error } = await fetchUserData(id);
+      setResponse(response as User);
+      setError(error);
     };
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { response, error };
+};
+
+export const fetchUserData = async (id?: number) => {
+  try {
+    logger.debug(`Fetching user: ${id}`);
+    const { data } = !id
+      ? await axiosClient.post('/users/me')
+      : await axiosClient.get(`/users/${id}`);
+    logger.debug('Got user: ', data);
+    const { data: followedUsers } = await axiosClient.get(
+      `/followers/following?userId=${data.id}`,
+    );
+    logger.debug('followed users:', followedUsers.rows);
+    return {
+      response: { ...data, followedUsers: followedUsers.rows },
+      error: null,
+    };
+  } catch (err: any) {
+    logger.error('An error ocurred while fetching the user: ', { error: err });
+    return { response: null, error: err };
+  }
 };
