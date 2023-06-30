5f56e626ed09904634a769cf336164d9d2bea141
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2023-06-30 13:12:19 -0300

    fix x

--- src/utils/workout-types.ts
+++ src/utils/workout-types.ts
@@ -16,25 +16,22 @@ export enum CategoryType {
 }
 
 export const categoryMap = new Map<number, string>([
-  [0, 'Piernas'], x
-  [1, 'Pecho'], x
-  [2, 'Espalda'], x
-  [3, 'Hombros'], x
-  [4, 'Brazos'], x
-  [5, 'Core'], x
-  [6, 'Cardio'], x
-  [7, 'Fullbody'], x
-  [8, 'Peso Libre'], x
-  [9, 'Estiramiento'], x
+  [0, 'Piernas'],
+  [1, 'Pecho'],
+  [2, 'Espalda'],
+  [3, 'Hombros'],
+  [4, 'Brazos'],
+  [5, 'Core'],
+  [6, 'Cardio'],
+  [7, 'Fullbody'],
+  [8, 'Peso Libre'],
+  [9, 'Estiramiento'],
   [10, 'Fuerza'],
   [-1, 'Cualquiera'],
 ]);
 
 export const imageMap = new Map<number, string>([
-  [
-    0,
-    '../imgs/category0.png',
-  ],
+  [0, '../imgs/category0.png'],
   [
     1,
     'https://static.vecteezy.com/system/resources/previews/006/417/775/original/man-doing-plank-abdominals-exercise-flat-illustration-isolated-on-white-background-free-vector.jpg',
