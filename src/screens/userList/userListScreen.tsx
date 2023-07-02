7ad32cad93661631e3480313880d59072caa9b41
Ezequiel Schejtman <ZequiceteYT@gmail.com>
2023-07-02 20:24:41 -0300

    Fix UserList Img

--- src/screens/userList/userListScreen.tsx
+++ src/screens/userList/userListScreen.tsx
@@ -40,8 +40,7 @@ const UserListScreen = ({ route }: UserListScreennProps) => {
         title: `${result.firstName} ${result.lastName}`,
         content: result.email,
         type: result.role,
-        imageUrl:
-          'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80',
+        imageUrl: require('../../imgs/user.png'),
       }),
     );
   };
